96f3f3c8d42865a8fcafff8e755057a412812fa6
13125006127 <771276847@qq.com>
2021-05-15 22:47:17 +0800

    public-welfare-preview

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import 'lib-flexible/flexible.js'
-import './assets/css/reset.css'
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
